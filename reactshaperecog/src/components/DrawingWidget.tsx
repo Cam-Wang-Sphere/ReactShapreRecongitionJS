5d6acfdc0517fb38c1a663ce042e5e3e443d72fa
Cam-Wang-Sphere <cam.wang@thesphere.com>
2024-08-12 17:26:01 -0700

    First pass drawing widget (not done!)

--- reactshaperecog/src/components/DrawingWidget.tsx
+++ reactshaperecog/src/components/DrawingWidget.tsx
@@ -1,28 +1,153 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-// interface DrawingProps
-// {
+interface DrawingProps
+{
+    drawEndFunction: () => void;
+}
 
-// }
+const UserInputKey = "UserInput";
 
-// const DrawingWidget = ( {} : DrawingProps) => {
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      e.name === "QuotaExceededError" &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
+  }
+}
+
+function AddNewInput(X : number, Y : number) {
+  let newInput = {
+    Input: [{ x: X, y: Y }],
+  };
+
+  let newInputString = JSON.stringify(newInput);
+  localStorage.setItem(UserInputKey, newInputString);
+}
+
+function AppendNexInput(X : number, Y : number) {
+  let existingInputString = localStorage.getItem(UserInputKey);
+
+  if(typeof existingInputString === 'string')
+  {
+    let existingInputObj = JSON.parse(existingInputString);
+    existingInputObj["Input"].push({ x: X, y: Y });
+  
+    let newInputString = JSON.stringify(existingInputObj);
+  
+    localStorage.setItem(UserInputKey, newInputString);
+  }
+}
+
+const canvasWidth = window.innerHeight;
+const canvasHeight = window.innerHeight;
+
+
+const DrawingWidget = ( {drawEndFunction} : DrawingProps) => {
  
+    const canvasRef = useRef(null);
+    const ctxRef = useRef(null);
+    const canvasRect = useRef(null);
+    const [isDrawing, setIsDrawing] = useState(false);
+    const [lineWidth /*setLineWidth*/] = useState(5);
+    const [lineColor, setLineColor] = useState("black");
+    const [lineOpacity /*setLineOpacity*/] = useState(1.0);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+        ctx.lineCap = "round";
+        ctx.lineJoin = "round";
+        ctx.globalAlpha = lineOpacity;
+        ctx.strokeStyle = lineColor;
+        ctx.lineWidth = lineWidth;
+        ctxRef.current = ctx;
+        canvasRect.current = canvas.getBoundingClientRect();
+    }, [lineColor, lineOpacity, lineWidth]);
+  // Function for starting the drawing
+ const startDrawing = (e) => {
+    // Reseting all the info
+    ctxRef.current.clearRect(
+      0,
+      0,
+      canvasRef.current.width,
+      canvasRef.current.height
+    );
+    if (storageAvailable("localStorage")) {
+      localStorage.removeItem(UserInputKey);
+    }
+    let x = e.touches
+      ? e.touches[0].clientX - canvasRect.current.left
+      : e.nativeEvent.offsetX;
+    let y = e.touches
+      ? e.touches[0].clientY - canvasRect.current.top
+      : e.nativeEvent.offsetY;
+    ctxRef.current.beginPath();
+    ctxRef.current.moveTo(x, y);
+    setIsDrawing(true);
+  };
+
+  // Function for ending the drawing
+  const endDrawing = () => {
+    ctxRef.current.closePath();
+    setIsDrawing(false);
+
+    drawEndFunction();
+  };
+
+  const draw = (e) => {
+    if (!isDrawing) {
+      return;
+    }
+    let x = e.touches
+      ? e.touches[0].clientX - canvasRect.current.left
+      : e.nativeEvent.offsetX;
+    let y = e.touches
+      ? e.touches[0].clientY - canvasRect.current.top
+      : e.nativeEvent.offsetY;
+    if (storageAvailable("localStorage")) {
+      // If no user input exists create a new one
+      if (!localStorage.getItem(UserInputKey)) {
+        AddNewInput(x, y);
+      }
+      // If it exists append to existing data
+      else {
+        AppendNexInput(x, y);
+      }
+    }
+
+    ctxRef.current.lineTo(x, y);
+
+    ctxRef.current.stroke();
+  }; 
 
 
 
-//     return (
-//         <div className="draw-area">
-//             <canvas
-//                 onMouseDown={startDrawing}
-//                 onMouseUp={endDrawing}
-//                 onMouseMove={draw}
-//                 onTouchStart={startDrawing}
-//                 onTouchEnd={endDrawing}
-//                 onTouchMove={draw}
-//                 ref={canvasRef}
-//                 width={canvasWidth}
-//                 height={canvasHeight}
-//             />
-//         </div>
-//     )
-// }
+    return (
+        <div className="draw-area">
+            <canvas
+                onMouseDown={startDrawing}
+                onMouseUp={endDrawing}
+                onMouseMove={draw}
+                onTouchStart={startDrawing}
+                onTouchEnd={endDrawing}
+                onTouchMove={draw}
+                ref={canvasRef}
+                width={canvasWidth}
+                height={canvasHeight}
+            />
+        </div>
+    )
+}
+
+export default DrawingWidget;
