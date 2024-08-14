8b0b19a2880b92daa36cddbe2c81634a87a140e4
architk94 <30574912+architk94@users.noreply.github.com>
2024-08-13 17:20:55 -0700

    success overlay to border

--- reactshaperecog/src/components/SuccessOverlay.tsx
+++ reactshaperecog/src/components/SuccessOverlay.tsx
@@ -1,83 +1,79 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { NetworkingManager } from "./../networking/NetworkingManager";
-import { Message } from '../schema/dot-dschema/message';
+import { Message } from "../schema/dot-dschema/message";
 
-import SucessSound from '../assets/sounds/SuccessSound.wav';
-import FailSound from "../assets/sounds/IncorrectSound.wav"
+import SucessSound from "../assets/sounds/SuccessSound.wav";
+import FailSound from "../assets/sounds/IncorrectSound.wav";
 
-interface SuccessOverlayProps
-{
-    inNetworkingManager: NetworkingManager | null;
+interface SuccessOverlayProps {
+  inNetworkingManager: NetworkingManager | null;
 }
 
-const SuccessOverlay = ({ inNetworkingManager }: SuccessOverlayProps) =>
-{
-    const [isVisible, setIsVisible] = useState(false);
+const SuccessOverlay = ({ inNetworkingManager }: SuccessOverlayProps) => {
+  const [isVisible, setIsVisible] = useState(true);
 
-    // @TODO NATHAN: cvar?
-    const flashBangTime = 200; // in milliseconds
-    const hapticTime = 100; // in milliseconds
-    const shouldPlaySounds = true;
+  // @TODO NATHAN: cvar?
+  const flashBangTime = 200; // in milliseconds
+  const hapticTime = 100; // in milliseconds
+  const shouldPlaySounds = true;
 
-    useEffect(() =>
-    {
-        const handleEvent = (inScore: number) =>
-        {
-            if (inScore < 0)
-            {
-                if (shouldPlaySounds)
-                {
-                    const failAudio = new Audio(FailSound);
-                    failAudio.play();
-                }
+  useEffect(() => {
+    const handleEvent = (inScore: number) => {
+      if (inScore < 0) {
+        if (shouldPlaySounds) {
+          const failAudio = new Audio(FailSound);
+          failAudio.play();
+        }
 
-                return;
-            }
+        return;
+      }
 
-            // playing haptics if possible
-            if ('vibrate' in navigator)
-            {
-                navigator.vibrate(hapticTime);
-            }
+      // playing haptics if possible
+      if ("vibrate" in navigator) {
+        navigator.vibrate(hapticTime);
+      }
 
-            // play sounds if possible
-            if (shouldPlaySounds)
-            {
-                const successAudio = new Audio(SucessSound);
-                successAudio.play();
-            }
+      // play sounds if possible
+      if (shouldPlaySounds) {
+        const successAudio = new Audio(SucessSound);
+        successAudio.play();
+      }
 
-            setIsVisible(true);
-            setTimeout(() =>
-            {
-                setIsVisible(false);
-            }, flashBangTime);
-        };
+      setIsVisible(true);
+      setTimeout(() => {
+        setIsVisible(false);
+      }, flashBangTime);
+    };
 
-        inNetworkingManager?.on(Message.ScoreUpdateResponse.toString(), handleEvent)
+    inNetworkingManager?.on(
+      Message.ScoreUpdateResponse.toString(),
+      handleEvent
+    );
 
-        // cleaning up
-        return () =>
-        {
-            inNetworkingManager?.off(Message.ScoreUpdateResponse.toString(), handleEvent);
-        };
-    }, [inNetworkingManager]);
+    // cleaning up
+    return () => {
+      inNetworkingManager?.off(
+        Message.ScoreUpdateResponse.toString(),
+        handleEvent
+      );
+    };
+  }, [inNetworkingManager]);
 
+  const greenScreenStyle = {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100vw",
+    height: "100vh",
+    border: "1.5rem solid",
+    borderColor: "##7aff73",
+    backgroundColor: "rgba(0, 128, 0, 0)", // last parameter is translucency
+    display: isVisible ? "block" : "none",
+    zIndex: 9999,
+    pointerEvents: "none",
+  } as const;
 
-    const greenScreenStyle = 
-    {
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100vw',
-        height: '100vh',
-        backgroundColor: 'rgba(0, 128, 0, 0.6)',  // last parameter is translucency
-        display: isVisible ? 'block' : 'none',
-        zIndex: 9999,
-        pointerEvents: 'none',
-    } as const;
-
-    return <div style={greenScreenStyle}></div>;
-}
+  return <div style={greenScreenStyle}></div>;
+};
 
 export default SuccessOverlay;
