1bbb3569f99ffd88b335a503c597db302bd221c0
Agustin <inaki.agustin@thesphere.com>
2024-08-15 10:31:02 -0700

    im crazy. cam and nathan in meeting. balls to walls. adding type parameters and null checks since one of the test suites is failing

--- reactshaperecog/src/components/DrawingWidget.tsx
+++ reactshaperecog/src/components/DrawingWidget.tsx
@@ -10,7 +10,7 @@ interface DrawingProps {
 
 const UserInputKey = "UserInput";
 
-function storageAvailable(type) {
+function storageAvailable(type: 'localStorage' | 'sessionStorage') {
   let storage;
   try {
     storage = window[type];
@@ -57,10 +57,10 @@ const canvasHeight = window.innerHeight;
 const DrawingWidget = ({
   drawEndFunction,
   inNetworkingManager,
-}: DrawingProps) => {
-  const canvasRef = useRef(null);
-  const ctxRef = useRef(null);
-  const canvasRect = useRef(null);
+  }: DrawingProps) => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
+  const canvasRect = useRef<DOMRect | null>(null);
   const [isDrawing, setIsDrawing] = useState(false);
   const [lineWidth /*setLineWidth*/] = useState(5);
   const [lineColor, setLineColor] = useState("black");
@@ -90,14 +90,18 @@ const DrawingWidget = ({
     );
 
     const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    ctx.lineCap = "round";
-    ctx.lineJoin = "round";
-    ctx.globalAlpha = lineOpacity;
-    ctx.strokeStyle = lineColor;
-    ctx.lineWidth = lineWidth;
-    ctxRef.current = ctx;
-    canvasRect.current = canvas.getBoundingClientRect();
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        ctx.lineCap = "round";
+        ctx.lineJoin = "round";
+        ctx.globalAlpha = lineOpacity;
+        ctx.strokeStyle = lineColor;
+        ctx.lineWidth = lineWidth;
+        ctxRef.current = ctx;
+        canvasRect.current = canvas.getBoundingClientRect();
+      }
+    }
 
     return () => {
       inNetworkingManager?.off(
@@ -108,23 +112,32 @@ const DrawingWidget = ({
   }, [lineColor, lineOpacity, lineWidth, inNetworkingManager]);
 
   // Function for starting the drawing
-  const startDrawing = (e) => {
+  const startDrawing = (e: React.MouseEvent<HTMLCanvasElement> | React.TouchEvent<HTMLCanvasElement>) => {
+    if (!ctxRef.current || !canvasRect.current) {
+      return;
+    }
+
     // Reseting all the info
-    ctxRef.current.clearRect(
-      0,
-      0,
-      canvasRef.current.width,
-      canvasRef.current.height
-    );
+    if (canvasRef.current) {
+      ctxRef.current.clearRect(
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
+    }
+
     if (storageAvailable("localStorage")) {
       localStorage.removeItem(UserInputKey);
     }
-    let x = e.touches
-      ? e.touches[0].clientX - canvasRect.current.left
-      : e.nativeEvent.offsetX;
-    let y = e.touches
-      ? e.touches[0].clientY - canvasRect.current.top
-      : e.nativeEvent.offsetY;
+
+    let x = ('touches' in e)
+      ? (e as React.TouchEvent<HTMLCanvasElement>).touches[0].clientX - canvasRect.current.left 
+      : (e.nativeEvent as MouseEvent).offsetX;
+    let y = ('touches' in e)
+      ? (e as React.TouchEvent<HTMLCanvasElement>).touches[0].clientY - canvasRect.current.top
+      : (e.nativeEvent as MouseEvent).offsetY;
+
     ctxRef.current.beginPath();
     ctxRef.current.moveTo(x, y);
     setIsDrawing(true);
@@ -132,22 +145,24 @@ const DrawingWidget = ({
 
   // Function for ending the drawing
   const endDrawing = () => {
-    ctxRef.current.closePath();
+    ctxRef.current?.closePath();
     setIsDrawing(false);
 
     drawEndFunction();
   };
 
-  const draw = (e) => {
-    if (!isDrawing) {
+  const draw = (e: React.MouseEvent<HTMLCanvasElement> | React.TouchEvent<HTMLCanvasElement>) => {
+    if (!isDrawing || !ctxRef.current || !canvasRect.current) {
       return;
     }
-    let x = e.touches
-      ? e.touches[0].clientX - canvasRect.current.left
-      : e.nativeEvent.offsetX;
-    let y = e.touches
-      ? e.touches[0].clientY - canvasRect.current.top
-      : e.nativeEvent.offsetY;
+
+    let x = ('touches' in e)
+      ? (e as React.TouchEvent<HTMLCanvasElement>).touches[0].clientX - canvasRect.current.left
+      : (e.nativeEvent as MouseEvent).offsetX;
+    let y = ('touches' in e)
+      ? (e as React.TouchEvent<HTMLCanvasElement>).touches[0].clientY - canvasRect.current.top
+      : (e.nativeEvent as MouseEvent).offsetY;
+    
     if (storageAvailable("localStorage")) {
       // If no user input exists create a new one
       if (!localStorage.getItem(UserInputKey)) {
@@ -159,10 +174,9 @@ const DrawingWidget = ({
       }
     }
 
-    ctxRef.current.lineTo(x, y);
-
+    ctxRef.current.lineTo(x, y);   
     ctxRef.current.stroke();
-  };
+  }; 
 
   return (
     <div className="draw-area">
