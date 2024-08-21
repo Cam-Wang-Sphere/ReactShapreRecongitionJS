9c8ad3b1b3953f2c390c4d720667052d71b2b52a
architk94 <30574912+architk94@users.noreply.github.com>
2024-08-21 16:55:25 -0700

    TNS canvas size relative to parent

--- reactshaperecog/src/components/TapnSlashInput.tsx
+++ reactshaperecog/src/components/TapnSlashInput.tsx
@@ -12,8 +12,7 @@ interface TapnSlashProps {
 const canvasWidth = window.innerHeight;
 const canvasHeight = window.innerHeight;
 
-const TapnSlashInput = ({inNetworkingManager} : TapnSlashProps) => {
-
+const TapnSlashInput = ({ inNetworkingManager }: TapnSlashProps) => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
   const canvasRect = useRef<DOMRect | null>(null);
@@ -35,100 +34,125 @@ const TapnSlashInput = ({inNetworkingManager} : TapnSlashProps) => {
     }
   }, [inNetworkingManager]);
 
-  const startDrawing = (e: React.MouseEvent<HTMLCanvasElement> | React.TouchEvent<HTMLCanvasElement>) => {
+  const startDrawing = (
+    e: React.MouseEvent<HTMLCanvasElement> | React.TouchEvent<HTMLCanvasElement>
+  ) => {
     if (!ctxRef.current || !canvasRect.current) {
       return;
     }
 
     setIsDrawing(true);
 
-    let x = ('touches' in e)
-    ? (e as React.TouchEvent<HTMLCanvasElement>).touches[0].clientX - canvasRect.current.left 
-    : (e.nativeEvent as MouseEvent).offsetX;
-    let y = ('touches' in e)
-    ? (e as React.TouchEvent<HTMLCanvasElement>).touches[0].clientY - canvasRect.current.top
-    : (e.nativeEvent as MouseEvent).offsetY;
+    let x =
+      "touches" in e
+        ? (e as React.TouchEvent<HTMLCanvasElement>).touches[0].clientX -
+          canvasRect.current.left
+        : (e.nativeEvent as MouseEvent).offsetX;
+    let y =
+      "touches" in e
+        ? (e as React.TouchEvent<HTMLCanvasElement>).touches[0].clientY -
+          canvasRect.current.top
+        : (e.nativeEvent as MouseEvent).offsetY;
 
     x /= canvasRect.current.width;
     y /= canvasRect.current.height;
 
     console.log("X: " + x + "Y: " + y);
 
-    let Event : ETriggerEvent = ETriggerEvent.Started;
-    let Pos : Vector2 = new Vector2(x,y);
-    let DateTime : Date = new Date()
-    let Time : number = DateTime.getTime();
-    let Handle : FTIMMappedAreaHandle = new FTIMMappedAreaHandle(0);
+    let Event: ETriggerEvent = ETriggerEvent.Started;
+    let Pos: Vector2 = new Vector2(x, y);
+    let DateTime: Date = new Date();
+    let Time: number = DateTime.getTime();
+    let Handle: FTIMMappedAreaHandle = new FTIMMappedAreaHandle(0);
 
-    let NewInput : FTIMInputEvent = new FTIMInputEvent(Handle, 0, Pos, Event, Time);
+    let NewInput: FTIMInputEvent = new FTIMInputEvent(
+      Handle,
+      0,
+      Pos,
+      Event,
+      Time
+    );
 
-    let Inputs : FTIMInputEvent[] = [];
+    let Inputs: FTIMInputEvent[] = [];
     Inputs.push(NewInput);
     inNetworkingManager?.sendTIMInputEvents(Inputs);
-
-
   };
 
   const endDrawing = () => {
     setIsDrawing(false);
 
-    let Event : ETriggerEvent = ETriggerEvent.Completed;
-    let Pos : Vector2 = new Vector2(0,0);
-    let DateTime : Date = new Date()
-    let Time : number = DateTime.getTime();
-    let Handle : FTIMMappedAreaHandle = new FTIMMappedAreaHandle(0);
+    let Event: ETriggerEvent = ETriggerEvent.Completed;
+    let Pos: Vector2 = new Vector2(0, 0);
+    let DateTime: Date = new Date();
+    let Time: number = DateTime.getTime();
+    let Handle: FTIMMappedAreaHandle = new FTIMMappedAreaHandle(0);
 
-    let NewInput : FTIMInputEvent = new FTIMInputEvent(Handle, 0, Pos, Event, Time);
+    let NewInput: FTIMInputEvent = new FTIMInputEvent(
+      Handle,
+      0,
+      Pos,
+      Event,
+      Time
+    );
 
-    let Inputs : FTIMInputEvent[] = [];
+    let Inputs: FTIMInputEvent[] = [];
     Inputs.push(NewInput);
     inNetworkingManager?.sendTIMInputEvents(Inputs);
   };
 
-  const draw = (e: React.MouseEvent<HTMLCanvasElement> | React.TouchEvent<HTMLCanvasElement>) => {
+  const draw = (
+    e: React.MouseEvent<HTMLCanvasElement> | React.TouchEvent<HTMLCanvasElement>
+  ) => {
     if (!isDrawing || !ctxRef.current || !canvasRect.current) {
       return;
     }
-    let x = ('touches' in e)
-    ? (e as React.TouchEvent<HTMLCanvasElement>).touches[0].clientX - canvasRect.current.left 
-    : (e.nativeEvent as MouseEvent).offsetX;
-    let y = ('touches' in e)
-    ? (e as React.TouchEvent<HTMLCanvasElement>).touches[0].clientY - canvasRect.current.top
-    : (e.nativeEvent as MouseEvent).offsetY;
+    let x =
+      "touches" in e
+        ? (e as React.TouchEvent<HTMLCanvasElement>).touches[0].clientX -
+          canvasRect.current.left
+        : (e.nativeEvent as MouseEvent).offsetX;
+    let y =
+      "touches" in e
+        ? (e as React.TouchEvent<HTMLCanvasElement>).touches[0].clientY -
+          canvasRect.current.top
+        : (e.nativeEvent as MouseEvent).offsetY;
 
     x /= canvasRect.current.width;
     y /= canvasRect.current.height;
 
     console.log("X: " + x + "Y: " + y);
-    
-    let Event : ETriggerEvent = ETriggerEvent.Ongoing;
-    let Pos : Vector2 = new Vector2(x,y);
-    let DateTime : Date = new Date()
-    let Time : number = DateTime.getTime();
-    let Handle : FTIMMappedAreaHandle = new FTIMMappedAreaHandle(0);
 
-    let NewInput : FTIMInputEvent = new FTIMInputEvent(Handle, 0, Pos, Event, Time);
+    let Event: ETriggerEvent = ETriggerEvent.Ongoing;
+    let Pos: Vector2 = new Vector2(x, y);
+    let DateTime: Date = new Date();
+    let Time: number = DateTime.getTime();
+    let Handle: FTIMMappedAreaHandle = new FTIMMappedAreaHandle(0);
 
-    let Inputs : FTIMInputEvent[] = [];
+    let NewInput: FTIMInputEvent = new FTIMInputEvent(
+      Handle,
+      0,
+      Pos,
+      Event,
+      Time
+    );
+
+    let Inputs: FTIMInputEvent[] = [];
     Inputs.push(NewInput);
     inNetworkingManager?.sendTIMInputEvents(Inputs);
   };
 
   return (
-    <Box
-            h="150px"
-            w="100%"
-            bg="white"
-        >
-          <canvas
-            onMouseDown={startDrawing}
-            onMouseUp={endDrawing}
-            onMouseMove={draw}
-            onTouchStart={startDrawing}
-            onTouchEnd={endDrawing}
-            onTouchMove={draw}
-            ref={canvasRef}
-          />
+    <Box h="400px" w="100%" bg="white">
+      <canvas
+        style={{ position: "relative", width: "100%", height: "100%" }}
+        onMouseDown={startDrawing}
+        onMouseUp={endDrawing}
+        onMouseMove={draw}
+        onTouchStart={startDrawing}
+        onTouchEnd={endDrawing}
+        onTouchMove={draw}
+        ref={canvasRef}
+      />
     </Box>
   );
 };
