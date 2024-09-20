f9ce366ca3d2eadcac5bf2f87a98a253119d3970
Nathan Barquira <nathan.barquira@thesphere.com>
2024-09-20 15:03:22 -0700

    WIP for forever fixing screen switching issue

--- reactshaperecog/src/components/GameState.tsx
+++ reactshaperecog/src/components/GameState.tsx
@@ -3,6 +3,7 @@ import { WSPlayerData } from "../player/WSPlayerData";
 import { NetworkingManager } from "../networking/NetworkingManager";
 import { Message } from "../schema/WSSchema";
 import { EWSPhaseEnums } from "../schema/ewsphase-enums";
+import { EWSGlobalInputTypes } from "../schema/ewsglobal-input-types";
 
 interface GameStateProps {
   inPlayerData: WSPlayerData | null;
@@ -25,10 +26,16 @@ const GameState = ({ inPlayerData, inNetworkingManager }: GameStateProps) =>
           inPlayerData?.setCurrentPhase(inPhase);
           console.log('set the players current phase to = ', inPhase, ' in the gamestate handler');
         }
+        const handleGlobalInputResponse = (inGlobalInput: EWSGlobalInputTypes) =>
+        {
+          inPlayerData?.setCurrentInput(inGlobalInput);
+          console.log('set the players current input to = ', inGlobalInput, ' in the gamestate handler');
+        }
 
         // bindings
         inNetworkingManager?.on(Message.ClientLoginResponse.toString(), handleLoginResponse);
         inNetworkingManager?.on(Message.PhaseResponse.toString(), handlePhaseResponse);
+        // inNetworkingManager?.on(Message.GlobalInputResponse.toString(), handleGlobalInputResponse);
         // inNetworkingManager?.on(...) continue here...
 
         // cleaning up
@@ -36,6 +43,7 @@ const GameState = ({ inPlayerData, inNetworkingManager }: GameStateProps) =>
         {
             inNetworkingManager?.off(Message.ClientLoginResponse.toString(), handleLoginResponse);
             inNetworkingManager?.off(Message.PhaseResponse.toString(), handlePhaseResponse);
+            // inNetworkingManager?.off(Message.GlobalInputResponse.toString(), handleGlobalInputResponse);
             // inNetworkingManager?.off(...) continue here...
         };
     }, [inNetworkingManager]);
